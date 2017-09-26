8fae788e1767fe949807ccc6eec201681db8f447
David Jetelina <david.jetelina@firma.seznam.cz>
2017-09-26 15:14:53 +0200

    fixes to setup.py
    Signed-off-by: David Jetelina <david.jetelina@firma.seznam.cz>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 Flask-FastRPC
 -------------
 
-Flask extension that allows it to be used as a fastrpc/xmlrpc server.
+Flask extension that allows it to be used as a fastrpc/xmlrpc server - please visit the HomePage for full readme
 """
 import sys
 from setuptools import setup
@@ -18,7 +18,7 @@ if sys.version_info.minor < 5:
 setup(
     name='Flask-FastRPC',
     version='1.4',
-    url='https://github.com/seznam',
+    url='https://github.com/iScrE4m/Flask-FastRPC',
     license='BSD',
     author='David Jetelina',
     author_email='david.jetelina@firma.seznam.cz',
